883fe427ca385c3035d48760de341c741caf2449
Dominique McLemore <macmanco85@gmail.com>
2019-07-17 11:36:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ function sayHiToGrandma(string){
   if (string === lowercase){
     return 'I can\'t hear you!'
   }else if (string === uppercase){
+<<<<<<< HEAD
   }else if (string === string.toUpperCase()){
+=======
+>>>>>>> 16a21a8483a6cccff7e196cac7112a5e6f236498
     return 'YES INDEED!'
   }else if (string === 'I love you, Grandma.'){
     return 'I love you, too.'
